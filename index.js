6d9682bea5fa38952cf5ef39d781c13e3862f9d4
Anastasia <nastasia199126@gmail.com>
2021-06-21 23:59:52 +0300

    m

---
+++ index.js
@@ -0,0 +1,42 @@
+class CountdownTimer {
+  constructor({ targetDate }) {
+    this.targetDate = targetDate;
+    this.refs = {
+      conteiner: document.querySelector('#timer-1'),
+      deis: document.querySelector('[data-value="days"]'),
+      hours: document.querySelector('[data-value="hours"]'),
+      mins: document.querySelector('[data-value="mins"]'),
+      secs: document.querySelector('[data-value="secs"]'),
+    };
+  }
+
+  calculateData(time) {
+    const days = Math.floor(time / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+    const secs = Math.floor((time % (1000 * 60)) / 1000);
+    return { days, hours, mins, secs };
+  }
+
+  textContent({ days, hours, mins, secs }) {
+    this.refs.deis.textContent = days;
+    this.refs.hours.textContent = hours;
+    this.refs.mins.textContent = mins;
+    this.refs.secs.textContent = secs;
+  }
+
+  start() {
+    setInterval(() => {
+      const curentData = Date.now();
+      const differenceData = this.targetDate - curentData;
+      const timeObject = this.calculateData(differenceData);
+      this.textContent(timeObject);
+    }, 1000);
+  }
+}
+
+const start = new CountdownTimer({
+  targetDate: new Date('Jul 17, 2021'),
+});
+
+start.start();
